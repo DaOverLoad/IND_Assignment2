925a1e2ac7e6e357ab446249a7caf469061f839a
DaOverLoad <44761075+DaOverLoad@users.noreply.github.com>
2019-01-21 11:50:34 +0000

    Add files via upload

---
+++ CollectiblesPick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectiblesPick : MonoBehaviour {
+
+    private AudioSource source;
+	[SerializeField] private AudioClip collectSound;
+
+	void OnTriggerEnter (Collider col) {
+		Debug.Log ("You've collected: " + gameObject.name);
+		source = col.GetComponent<AudioSource> ();
+		source.PlayOneShot (collectSound, 1f);
+		Destroy (gameObject);
+	}
+}
